4bafc9b4b1a817b5a48821b786f59792911f33b3
Bucky1245 <58512405+Bucky1245@users.noreply.github.com>
2023-12-15 10:56:24 +0100

    card V2.0

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {Dimensions, Image, StyleSheet, View} from "react-native";
+import {Animated, Dimensions, Image, StyleSheet, TouchableOpacity, View} from "react-native";
 import colors from "../config/colors";
 import AppText from "./AppText";
+import Icon from "./Icon";
 
 const Card = (recipe) => {
-    // I want a card for a recipe with an image
+    const [expanded, setExpanded] = useState(false);
+    const opacityValue = useState(new Animated.Value(0))[0];
+
+    const toggleExpanded = () => {
+        setExpanded(!expanded);
+        Animated.timing(opacityValue, {
+            toValue: 1,
+            duration: 200,
+            useNativeDriver: true,
+        }).start();
+    };
+
     return (
         <View style={styles.container}>
-        <View style={styles.card}>
-            <Image style={styles.image} source={{uri: recipe.recipe.recipe.image}}/>
-            <View style={styles.detailsContainer}>
-                <AppText style={styles.title}>{recipe.recipe.recipe.label}</AppText>
-                <AppText style={styles.subTitle}>{recipe.recipe.recipe.source}</AppText>
-            </View>
-        </View>
+                <View style={styles.card}>
+                    <Image style={styles.image} source={{ uri: recipe.recipe.recipe.image }} />
+                    <View style={styles.detailsContainer}>
+                        <AppText style={styles.title}>{recipe.recipe.recipe.label}</AppText>
+                        <AppText style={styles.subTitle}>{recipe.recipe.recipe.source}</AppText>
+                        {expanded && (
+                            <View>
+                                <AppText>Ingredients:</AppText>
+                                {recipe.recipe.recipe.ingredients.map((ingredient, index) => (
+                                    <AppText style={styles.ingredientLines} key={index}>- {ingredient.text}</AppText>
+                                ))}
+                                <AppText>Recipe:</AppText>
+                                {recipe.recipe.recipe.instructionLines.map((instruction, index) => (
+                                    <AppText style={styles.ingredientLines} key={index}>Step {index}: {instruction}</AppText>
+                                ))}
+                            </View>
+                        )}
+                    </View>
+                    <View style={styles.arrowIcon}>
+                        <TouchableOpacity style={styles.touchableOpacity} onPress={() => toggleExpanded()}>
+                        <Icon backgroundColor="#FFFFFF" iconColor="#000000" name={expanded ? 'chevron-up' : 'chevron-down'} size={20} color={colors.primary} />
+                        </TouchableOpacity>
+                        </View>
+                </View>
         </View>
     );
 }
@@ -26,6 +55,17 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    touchableOpacity: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    arrowIcon: {
+        position: 'absolute',
+        right: 20,
+        bottom: 20,
+        transform: [{ scaleX: 2.5 }, { scaleY: 2.5 }],
     },
     card: {
         width: windowWidth * 0.9,
@@ -41,6 +81,12 @@ const styles = StyleSheet.create({
         shadowRadius: 6,
         elevation: 8,
         overflow: 'hidden',
+    },
+    ingredientLines: {
+        color: colors.secondary,
+        fontSize: 16,
+        fontWeight: '500',
+        marginLeft: 20,
     },
     detailsContainer: {
         padding: 20,
