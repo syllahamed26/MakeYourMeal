0f2400226a9d7aec1f55149e9784b09ba4457ee9
Hamed Cédric SYLLA <syllahamed26@yahoo.com>
2023-12-15 15:35:11 +0100

    update app.js

--- App.js
+++ App.js
@@ -3,16 +3,32 @@ import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import LoginScreen from "./app/screens/Account/LoginScreen";
 import MyTabs from "./app/components/BottomTabBar";
+import {useEffect, useState} from "react";
+import ForgotPasswordScreen from "./app/screens/Account/ForgotPasswordScreen";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
-  return (
+    const [initialRouteName, setInitialRouteName] = useState(null);
+    useEffect(() => {
+        const checkUser = async () => {
+            const user = await getUser();
+            if (user) {
+                setInitialRouteName('Home');
+            }else {
+                setInitialRouteName('Login');
+            }
+        }
+        checkUser();
+    }, []);
+
+    return initialRouteName ? (
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Navigator initialRouteName={initialRouteName} screenOptions={{headerShown: false}}>
             <Stack.Screen name="Login" component={LoginScreen}/>
             <Stack.Screen name="Register" component={RegisterScreen}/>
+            <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen}/>
             <Stack.Screen name="Home" component={MyTabs}/>
         </Stack.Navigator>
       </NavigationContainer>
-    );
+    ) : null;
 }
